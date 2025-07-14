a247367695a50e90ee3eea43c1fd200eb98368c6
abhishekdev8928 <abhishek.sharma@digihost.in>
2025-07-14 11:02:07 +0530

    Added mongodb url

--- utils/db.js
+++ utils/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const URI = "mongodb://127.0.0.1:27017/task-manager";
+const URI = "mongodb+srv://abhisheksharma:2hUsnH6khUfPSHDj@task-manager.ppwbavp.mongodb.net/task-manager";
 // const URI = "mongodb+srv://digihost2021:E1IBiNmZzhWtKDyB@cluster0.c1qye.mongodb.net/test";
 // mongoose.connect(URI);
 

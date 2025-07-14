aaf45664cef35deaf945aef2343eebe3f46b8363
abhishekdev8928 <abhishek.sharma@digihost.in>
2025-07-14 11:51:26 +0530

    Update Cors Origin

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const errorMiddleware1 = require("./middlewares/error-middleware");
 
 // ✅ CORS Configuration
 const corsOptions = {
-  origin: ['http://localhost:3000', 'http://localhost:3001', 'http://localhost:5173'],
+  origin: ["https://task-manager-frontend-eosin-five.vercel.app"],
   methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
   credentials: true,
 };
